02ffe31eb590aa504d3d80f11a3a0287b27fe843
khoivku <144471086+khoivku@users.noreply.github.com>
2023-09-16 00:38:00 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
